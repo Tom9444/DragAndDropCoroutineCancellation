6efa5dc7f8b273c98a9d85bc1123edf925a6aa48
Tom Anter <tomanter@Toms-MacBook-Pro.local>
2020-02-05 08:30:23 -0500

    Changed before presentation

--- app/src/main/java/com/example/draganddropcoroutinecancellation/MainActivity.kt
+++ app/src/main/java/com/example/draganddropcoroutinecancellation/MainActivity.kt
@@ -7,50 +7,35 @@ import android.view.MotionEvent
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.*
 
-
 // callbackFlow creates code flow
-suspend fun View.dragIt(): Flow<MotionEvent> = callbackFlow {
+suspend fun View.dragAndDrop(): Flow<MotionEvent> = callbackFlow {
 
-
-    this@dragIt.setOnTouchListener { _, event ->
+    this@dragAndDrop.setOnTouchListener { _, event ->
         offer(event)
     }
 
-    // called when canceled or closed
-    awaitClose { this@dragIt.setOnTouchListener(null) }
-
+    // called when cancelled or closed
+    awaitClose { this@dragAndDrop.setOnTouchListener(null) }
 }
-
-
-
-
 
 
 class MainActivity : AppCompatActivity() {
 
-
-
-
-    override fun onCreate(savedInstanceState: Bundle?)  {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val imageView = findViewById<ImageView>(R.id.image)
 
-
-
-
-
         // GlobalScope running pon application lifecycle
         // launch creates non blocking coroutine
-
         GlobalScope.launch {
 
-
-            val touch = imageView.dragIt()
+            val touch = imageView.dragAndDrop()
 
             val touchDownGesture =
                 touch.filter { motionEvent -> motionEvent.action == MotionEvent.ACTION_DOWN }
@@ -60,33 +45,27 @@ class MainActivity : AppCompatActivity() {
                 touch.filter { motionEvent -> motionEvent.action == MotionEvent.ACTION_UP }
 
 
+            touchDownGesture.flatMapConcat {
 
-            touchDownGesture.flatMapConcat { initialTouch ->
-
-                val firstCoordinate = Pair(initialTouch.x, initialTouch.y)
-                Log.i("tagg", "${initialTouch}")
-
-
+                val originalPosition = Pair(imageView.x , imageView.y)
 
                 moveGesture.map { moveCoordinate ->
+                    val x = moveCoordinate.x - originalPosition.first
+                    val y = moveCoordinate.y - originalPosition.second
 
-
-                    val x = moveCoordinate.x - firstCoordinate.first
-                    val y = moveCoordinate.y - firstCoordinate.second
                     Pair(x, y)
-
                 }
-
-
+            }
+                .flatMapLatest{
+                    touchUpGesture
+                    flow { emit(it) }
                 }
+//                .takeUntil(touchUpGesture)
                 .collect {
-
-                Log.i("tagg", "${it.first} ${it.second}")
-
-                imageView.x = imageView.x + it.first
-                imageView.y = imageView.y + it.second
-            }
-            }
+                    imageView.x += it.first
+                    imageView.y += it.second
+                }
         }
     }
+}
 
